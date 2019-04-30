d01498c309149caae1b34c0cce5807a0f67b0fe7
Elisha Marshall <iamelimars@yahoo.com>
2019-04-30 19:23:25 -0400

    Removed all console.logs

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://mongo:27017/radio-auth",
-  // mongoURI: 'mongodb://localhost/radio-auth',
+  // mongoURI: 'mongodb://localhost:27017/radio-auth',
   secretOrKey: "secret"
 }
