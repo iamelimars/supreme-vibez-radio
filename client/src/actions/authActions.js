0cd6fde2bf5fa92d3077b88f3130a74646275aac
Elisha Marshall <iamelimars@yahoo.com>
2019-04-30 19:52:24 -0400

    Corrected urls for production

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -11,7 +11,7 @@ import {
 //Register user
 export const registerUser = (userData, history) => dispatch => {
     axios
-        .post('http://localhost:5001/api/users/register', userData)
+        .post('http://192.241.133.63:5001/api/users/register', userData)
         .then(res => history.push('/login')) //re-direct to login pae on successful register
         .catch(err => 
                 dispatch({
@@ -24,7 +24,7 @@ export const registerUser = (userData, history) => dispatch => {
 //Login - get user token
 export const loginUser = userData => dispatch => {
     axios
-        .post("http://localhost:5001/api/users/login", userData)
+        .post("http://192.241.133.63:5001/api/users/login", userData)
         .then(res => {
             //save to local storage
 
