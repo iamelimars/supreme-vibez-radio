3422cb5072fdb5480b28e8ead096f5a2ce642564
Elisha Marshall <iamelimars@yahoo.com>
2019-05-02 21:46:42 -0400

    Added build folder

---
+++ client/build/precache-manifest.89f962e3e8e7ceb3999636afeef6348c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b525a31a1ae9e8e2f856be8e3996305e",
+    "url": "/static/media/chatting.b525a31a.svg"
+  },
+  {
+    "revision": "1a6244d73740da3f93d4",
+    "url": "/static/css/main.aaece62d.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dc82cf8b4bb2ecd78fa0",
+    "url": "/static/js/2.dc82cf8b.chunk.js"
+  },
+  {
+    "revision": "2fe82231b6105ce0979f0f3d6e3e0019",
+    "url": "/static/media/trianglify2.2fe82231.svg"
+  },
+  {
+    "revision": "5567b06858d89bd6be514bf1d59c4d59",
+    "url": "/static/media/authentication.5567b068.svg"
+  },
+  {
+    "revision": "94232f96aad707d2d2fbaf8d75b327d2",
+    "url": "/static/media/logo.94232f96.png"
+  },
+  {
+    "revision": "1a6244d73740da3f93d4",
+    "url": "/static/js/main.1a6244d7.chunk.js"
+  },
+  {
+    "revision": "bb753a4bc4e211b6081fad9c22a419d9",
+    "url": "/static/media/hero-2.bb753a4b.jpg"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "/static/media/apple.0f3514a4.svg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "78b5b5a60afec449b8348396d1418aea",
+    "url": "/static/media/android.78b5b5a6.svg"
+  },
+  {
+    "revision": "3977b36f5fdf6dc28084cf48b19b2ae1",
+    "url": "/static/media/group_chat.3977b36f.svg"
+  },
+  {
+    "revision": "e4b63a90dd35d3797b143b1a5838fec2",
+    "url": "/index.html"
+  }
+];
